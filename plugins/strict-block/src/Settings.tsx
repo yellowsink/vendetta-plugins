aac5a210969f94080a29a1e177f4daf7176a72f1
yellowsink <yellowsink@riseup.net>
2023-04-15 17:25:52 +0100

    ui

--- plugins/strict-block/src/Settings.tsx
+++ plugins/strict-block/src/Settings.tsx
@@ -1,4 +1,17 @@
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { getAssetIDByName } from "@vendetta/ui/assets";
+import {ReactNative} from "@vendetta/metro/common";
+const { FormSwitchRow, FormIcon } = Forms;
 
-export default () => <FormText>Hello, world!</FormText>;
+export default () => (
+	<ReactNative.ScrollView>
+	  <FormSwitchRow
+	    label="Also hide muted users"
+	    subLabel="Coming soon"
+	    leading={<FormIcon source={getAssetIDByName("ic_notif_off")} />}
+	    value={false}
+	    onValueChange={() => {}}
+	    disable
+	  />
+	</ReactNative.ScrollView>
+);
